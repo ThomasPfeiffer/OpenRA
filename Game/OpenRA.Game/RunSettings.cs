336183d16fa7bd3b63508d37e18ab528ff95c7aa
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2016-10-26 18:40:24 +0200

    runsettings for previous commits

--- Game/OpenRA.Game/RunSettings.cs
+++ Game/OpenRA.Game/RunSettings.cs
@@ -8,13 +8,14 @@ namespace OpenRA
 {
     public sealed class RunSettings
     {
-        public const bool Headless = true;
+        public const bool Headless = false;
         public const bool Autostart = true;
         public const int Max_Ticks = 200000;
-        public const int Timestep = 40; // Default 40
+        public const int Timestep = 1; // Default 40
         public const string Default_Mod = "";
-        public const string Default_Map = "ma_temperat";
-        public const string AI = "Naval AI";
+        public const string Default_Map = ""; // ma_temperat
+        public const string AI = "Rush AI"; // Normal AI, Rush AI, Naval AI, Turtle AI
+        public const string AI2 = "Rush AI"; // Normal AI, Rush AI, Naval AI, Turtle AI
 
     }
 }
