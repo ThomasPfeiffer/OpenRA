4809ea4d6e4874920c1fd666f0758d3ee5a04632
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2016-10-31 12:22:02 +0100

    restructuring

--- Scripts/setup/actors/change_actors.py
+++ Scripts/setup/actors/change_actors.py
@@ -1,5 +1,6 @@
-import yaml_util
-import csv_util
+from yaml_util import parse_yaml_file
+from yaml_util import dump_yaml
+from csv_util import read_actors
 from collections import OrderedDict
 
 map_path = "../Game/mods/ra/maps/"
@@ -7,13 +8,13 @@ map_name = "ma_temperat/"
 map_file = map_path + map_name + 'map.yaml'
 actors_file = map_path + map_name + 'actors.csv'
 
-actor_list = csv_util.read_actors(actors_file)
+actor_list = read_actors(actors_file)
 new_actor_dict = OrderedDict([('self', '')])
 i=0
 for actor in actor_list:
     new_actor_dict['Actor' + str(i)] = actor.as_dict()
     i += 1
 
-yaml = yaml_util.parse_yaml_file(map_file)
+yaml = parse_yaml_file(map_file)
 yaml['Actors'] = new_actor_dict
-yaml_util.dump_yaml(yaml,  map_file)
+dump_yaml(yaml,  map_file)
