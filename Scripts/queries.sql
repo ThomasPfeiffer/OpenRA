25bd33d00ea935cca4b11f76a6383efeea467bee
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2016-12-12 11:11:44 +0100

    new queries

--- Scripts/queries.sql
+++ Scripts/queries.sql
@@ -10,16 +10,14 @@ select ragame.run_id, count(*) from ragame group by ragame.run_id;
 select name, value, run_id,max_ticks_reached, fitness, raparameter.game_id  from raparameter join ragame, run where ragame.id == raparameter.game_id and run.id == ragame.run_id and run.id == 1;
 
 -- fitness list for a run
-select fitness from ragame join run where ragame.run_id == run.id and run.id ==20;
+select ragame.id, fitness from ragame where ragame.run_id =49;
 
 -- best game of a run
-select ragame.id, min(fitness) from ragame join run where ragame.run_id == run.id and run.id ==17;
+select ragame.id, min(fitness) from ragame join run where ragame.run_id == run.id and run.id =47;
 
 -- Game count for a run
 select count(*) from ragame where ragame.run_id == 16;
 
-select * from ragame where ragame.run_id is 13;
 
-update ragame set run_id = 13 where ragame.run_id is null;
-
-select * from raparameter where raparameter.file_string not like "param_weap%";
+-- Select run parameter list for excel analysis
+select ragame.game_id, fitness, name, value from raparameter join ragame on ragame.id = raparameter.game_id where run_id = 40;
