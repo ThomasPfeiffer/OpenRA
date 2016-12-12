c203270a80ffd246c1a83254cd20d7547c81dfd3
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2016-12-12 11:12:11 +0100

    reevaluate option

--- Scripts/balancing/evoalgosOptimization/selection.py
+++ Scripts/balancing/evoalgosOptimization/selection.py
@@ -1,8 +1,23 @@
 from evoalgos.selection import Selection
+from balancing.utility import log_util
+from balancing import settings
+
+LOG = log_util.get_logger(__name__)
 
 
 class SingleObjectiveSelection(Selection):
+    def __init__(self, objective_function):
+        self.objective_function  = objective_function
+
+    def reevaluate(self, individual):
+        LOG.debug("Reevaluating individual " + str(individual.id_number) + " with value " + str(individual.objective_values))
+        individual.objective_values = self.objective_function(individual.genome)
+        LOG.debug("Resulted in " + str(individual.objective_values))
+
     def reduce_to(self, population, number, already_chosen=None):
+        for indi in population:
+            if settings.reevaluate and indi.age > 0:
+                self.reevaluate(indi)
         sorted_pop = sorted(population, key=lambda individual: individual.objective_values, reverse=True)
         rejected = []
         i = 0
--- Scripts/balancing/settings.py
+++ Scripts/balancing/settings.py
@@ -10,7 +10,7 @@ game_log = workspace_path + "/logs/{0}_gen_openra.yaml".format(datetime.now().st
 
 # RA Game settings
 headless = True
-max_ticks = 200000
+max_ticks = 100000
 timestep = 1
 map_name = "ma_temperat"
 ai1 = "Rush AI"
@@ -18,12 +18,12 @@ ai1_faction = "england"
 ai2 = "Rush AI"
 ai2_faction = "ukraine"
 fitness_function_id = 1
-run_description = "Basic units, large parameter bounds. ESReproduction, popsize 10. Only asexual reproduction. 50 Generations."
-
+run_description = "Replay 5307"
 
 # Settings for execution without optimization
-games_to_play = 1000 # Amount of games played without changing parameters
-game_for_replay = None # If id is given reconstructs parameters from database to replay with same settings
+games_to_play = 50 # Amount of games played without changing parameters
+game_for_replay = 5307 # If id is given reconstructs parameters from database to replay with same settings
 
-popsize=10
-max_generations=50
+popsize=1
+max_generations=500
+reevaluate=True
