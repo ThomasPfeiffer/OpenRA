b009a38ff45aa51d91ffdf46e1c9fead84a942fd
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2016-12-15 11:19:07 +0100

    backup to sciebo

--- backup.bat
+++ backup.bat
@@ -5,3 +5,9 @@ set backupFilename=backup-%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%.db
 copy %sourceFile% "%backupFolder%%backupFilename%"
 
 if %errorlevel% neq 0 pause
+
+set sourceFolder2="C:\Users\Thomas\Google Drive\Uni\S11\Masterarbeit\*"
+set backupFolder2="C:\Users\Thomas\sciebo\Masterarbeit"
+xcopy /y /k /i /e %sourceFolder2% "%backupFolder2%"
+
+if %errorlevel% neq 0 pause
