f49d0663dd7e85d685cde0fa351ba875548a667c
Thomas <tpfeiffer@ZEBDEPNB1001044.zeb.de>
2016-12-27 12:24:10 +0100

    fix param parser

--- Scripts/balancing/utility/yaml_util.py
+++ Scripts/balancing/utility/yaml_util.py
@@ -95,7 +95,7 @@ def read_params_from_template(directory, template_file):
                     file_string= match.group(0),
                     min_value = float(s[1]),
                     max_value = float(s[2]),
-                    start_value= s[3] if len(s) > 3 else None
+                    start_value= float(s[3]) if len(s) > 3 else None
                 )
                 parameters.append(p)
     return parameters
