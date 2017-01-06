21a51f9d3d40f0eaf33c041c1a7fa6f01293ce11
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2017-01-06 18:29:14 +0100

    fixed logging

--- Scripts/balancing/utility/log_util.py
+++ Scripts/balancing/utility/log_util.py
@@ -5,16 +5,10 @@ formatter = logging.Formatter(fmt='%(asctime)10s%(levelname)8s: %(message)s', da
 loggers = []
 
 
-def add_handler(handler):
-    handler.setFormatter(formatter)
-    for logger in loggers:
-        logger.addHandler(handler)
-
-
 def get_logger(class_name):
     logger = logging.getLogger(class_name)
     ch = logging.StreamHandler()
-    add_handler(ch)
+    logger.addHandler(ch)
     logger.propagate = False
     logger.setLevel(logging.DEBUG)
     loggers.append(logger)
