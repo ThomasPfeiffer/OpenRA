a6ffd7d4d1b3ce502bd228023a2f1b5098d66af8
ThomasPfeiffer <git.ThomasPfeiffer@live.de>
2017-01-06 18:26:50 +0100

    reduced mutation

--- Scripts/balancing/evoalgosOptimization/individual.py
+++ Scripts/balancing/evoalgosOptimization/individual.py
@@ -43,7 +43,7 @@ class RandomMutationIndividual(StorableIndividual):
         new_genome = []
         for param in self.genome:
             new_param = param.clone()
-            mutated_value = (random.uniform(0.01, 0.10) * random.choice([-1, 1])+1) * param.value
+            mutated_value = (random.uniform(0.01, 0.05) * random.choice([-1, 1])+1) * param.value
             new_param.value = int(mutated_value)
             if new_param.value > new_param.max_value:
                 new_param.value = new_param.max_value
